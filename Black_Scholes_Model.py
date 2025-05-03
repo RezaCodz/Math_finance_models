5ec6a77ee107a399f5a71ffb7e8aa7300a19330d
soltanipour.mr <soltanipour.mr@gmail.com>
2025-05-03 13:47:08 +0200

    function traj

--- Black_Scholes_Model.py
+++ Black_Scholes_Model.py
@@ -1,36 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Parameters
-S0 = 100        
-mu = 0.05       
-sigma = 0.2
-T = 1.0        
-dt = 0.01       
-N = int(T / dt) 
-M = 1000          # number of paths
 
-# Time vector
-t = np.linspace(0, T, N + 1)
+def trajectory(S0 = 100, mu = 0.05, sigma = 0.2, T = 1.0, dt = 0.01,M = 1000):
+    # Parameters
+    S0 = 100        
+    mu = 0.05       
+    sigma = 0.2
+    T = 1.0        
+    dt = 0.01       
+    N = int(T / dt) 
+    M = 1000          # number of paths
 
-# Simulate GBM paths
-S = np.zeros((M, N + 1))
-S[:, 0] = S0
+    # Time vector
+    t = np.linspace(0, T, N + 1)
 
-for i in range(1, N + 1):
-    Z = np.random.standard_normal(M)  # standard normal random variables
-    S[:, i] = S[:, i - 1] * np.exp((mu - 0.5 * sigma ** 2) * dt + sigma * np.sqrt(dt) * Z)
+    # Simulate GBM paths
+    S = np.zeros((M, N + 1))
+    S[:, 0] = S0
 
-# Plot
-plt.figure(figsize=(10, 6))
-for j in range(M):
-    plt.plot(t, S[j], lw=1)
-plt.title("Geometric Brownian Motion Simulation")
-plt.xlabel("Time (Years)")
-plt.ylabel("Asset Price")
-plt.grid(True)
-plt.show()
+    for i in range(1, N + 1):
+        Z = np.random.standard_normal(M)  # standard normal random variables
+        S[:, i] = S[:, i - 1] * np.exp((mu - 0.5 * sigma ** 2) * dt + sigma * np.sqrt(dt) * Z)
 
-print("done")
-exit()
 
+    print(len(S))
+    # Plot
+    plt.figure(figsize=(10, 6))
+    for j in range(M):
+        plt.plot(t, S[j], lw=1)
+    plt.title("Geometric Brownian Motion Simulation")
+    plt.xlabel("Time (Years)")
+    plt.ylabel("Asset Price")
+    plt.grid(True)
+    plt.show()
+    print("done")
+    exit()
+
+trajectory()
